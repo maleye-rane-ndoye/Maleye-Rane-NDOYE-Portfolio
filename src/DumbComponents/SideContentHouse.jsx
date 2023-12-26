803e94b05b786401bf52588063ea90baa93e4454
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-26 15:58:42 +0100

    change image profile to fit. adjust button home . add css for description div in home page

--- src/DumbComponents/SideContentHouse.jsx
+++ src/DumbComponents/SideContentHouse.jsx
@@ -5,38 +5,45 @@ export default function SideContentHouse(){
 
     return(
     
-        <div className="Home-content">
-            <h4 className="mt-8 font-semibold text-white">Hello i am</h4>
-            <h1 className="mt-5 font-extrabold text-xl lg:text-5xl text-gray-100">Maleye Rane Ndoye</h1>
-            <div className="Home-content-img">
-                <div>
-                    
-                    <p className='w-full h-full text-white px-4 text-md font-bold '>
-                        I am web developper with extensive experience for over 3 years<br />
-                        My expertise is to create and website design, graphic design and many more ...
+        
+            <div className="Home-content">
+                    <div>
+                        <h1 className=" text-xl font-semibold text-gray-100  mt-10">Hello i am</h1>
+                        <h2 className=" font-extrabold text-2xl lg:text-3xl text-gray-100 ">Maleye Rane Ndoye</h2>
+                    </div>
 
-                    </p>
-                
-                </div>
-                <div className=' w-96 h-96 bg-gradient-to-r from-[#292448]  via-[#302b63]   to-[#171337] rounded-full flex flex-row items-center justify-center'>
-                     <img className="w-72 h-72 rounded-full" src={profile}/>
-                </div>
-                <div className=' flex flex-row space-x-7'>
-                    <button className=' w-32 h-10 rounded-2xl border-2 text-white text-lg font-semibold hover:scale-105'>see portfolio</button>
-                    <button className=' w-32 h-10 rounded-2xl border-2 text-white text-lg font-semibold hover:scale-105'>write me</button>
-                </div>
+                    <div className='w-full h-auto p-3 text-white font  font-light rounded border-2 m-0 mt-5 '>
+                      
+                            I am web developper with extensive experience 
+                            for over 3 years. My expertise is to create and website design, 
+                            graphic design and many more ...
+                      
+                    </div>
+
+                    <div className='w-full h-full m-0 flex flex-1 items-center justify-center'>
+                          <img className="w-72 h-72 rounded-full flex flex-row items-center" src={profile}/>
+                    </div>
+
+
+                    
+                    <div className='m-0 flex flex-row items-center justify-center space-x-7'>
+                        <button className=' w-32 h-10 m-0 rounded-2xl border-2 text-white text-lg font-semibold hover:scale-105'>see portfolio</button>
+                        <button className=' w-32 h-10 m-0 rounded-2xl border-2 text-white text-lg font-semibold hover:scale-105'>write me</button>
+                    </div>
+
+
+                    <div className="mt-12 flex flex-row items-center justify-center mb-5 space-x-24">
+                            <span className='text-white text-xl cursor-pointer hover:scale-125'><FaGithub/></span>
+                            <span className='text-white text-xl cursor-pointer hover:scale-125'><FaInstagram/></span>
+                            <span className='text-white text-xl cursor-pointer hover:scale-125'><FaLinkedin/></span>
+                    </div>
             </div>
 
 
 
             
-        </div>
+        
 
 
 )
 }
-{/* <div className="mt-12 flex flex-row space-x-24">
-            <span className='text-white text-xl cursor-pointer hover:text-cyan-200'><FaGithub/></span>
-            <span className='text-white text-xl cursor-pointer hover:text-cyan-200'><FaInstagram/></span>
-            <span className='text-white text-xl cursor-pointer hover:text-cyan-200'><FaLinkedin/></span>
-</div> */}
